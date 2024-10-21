460137ae5750fd5d63f3b8eb5706237dee1e0c5c
JustinasZailskas <justinaszailskas@gmail.com>
2024-10-21 22:39:54 +0300

    Vaziuoja iki kliuties, tada sustoja, nemato vel vaziuoja

--- movie_robot/main.py
+++ movie_robot/main.py
@@ -7,10 +7,8 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
-
 
 # Create your objects here.
 ev3 = EV3Brick()
@@ -19,10 +17,18 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
+# Inicijuojami jutikliai
+ultrasonicSensor = UltrasonicSensor(Port.S4)
+
 # Inicijuojama roboto važiuoklė.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
 # Write your program here.
-robot.straight(100)
 ev3.speaker.beep()
-robot.turn(90)
+while True:
+    if ultrasonicSensor.distance() > 250:
+        robot.drive(100,0)
+    else:
+        robot.stop() 
+        # robot.straight(-100)
+    
